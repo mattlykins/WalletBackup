57b174e2f29ac6c0bf12dfb80d89a83fc92d3749
mattlykins <matt@mattlykins.com>
2014-02-25 04:20:20 -0500

    Copy wallet files to encrypted zip

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-import glob
 import os
+import shutil
+import subprocess as sp
 basePath = (r"c:\users\maxwell\AppData\roaming")
 copyPath = (r"t:\BACKUP\BKW")
+zipPath = (r"C:\Program Files\7-Zip\7z.exe")
+password = "archivePassword"
+if not os.path.exists(copyPath):
+    os.makedirs(copyPath)
+    
 fileListFull = []
+outputBase = []
 for root, dirs, files in os.walk(basePath):
     for file in files:
         if "wallet" in file:
-             fileListFull.append(os.path.join(root,file))
+            fileListFull.append(os.path.join(root,file))
              
 for f in fileListFull:
-    print(f.split("\\")[-2])
+    outputBase.append(f.split("\\")[-2])
+    
+for copyFrom,copyTo in zip(fileListFull,outputBase):
+#    shutil.copyfile(copyFrom,copyPath+"\\"+copyTo+".dat")
+    rc=sp.call([zipPath, 'a', "-p"+password, '-y', copyPath+"\\"+copyTo+".7z"] + [copyFrom])
